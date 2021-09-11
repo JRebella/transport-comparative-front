11e31c5047be95f6678b1b5c8634fb2340300084
JRebella <jrebella96@gmail.com>
2021-09-11 15:26:02 -0300

    working location query

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,9 @@ import RoutePicker from "../components/RoutePicker/RoutePicker";
 import styles from "../styles/Home.module.css";
 
 import { noop } from "lodash";
+import { queryGeolocation } from "../api/AppPrices";
+
+queryGeolocation("26 de marzo 3394");
 
 const Home: NextPage = () => {
   return (
